b6fd82a79c2d108c40dbe6e56d9bed097293e3c2
4iar <4iar@users.noreply.github.com>
2016-08-03 20:13:11 +0100

    Implement yelp/location endpoint

--- server/app.js
+++ server/app.js
@@ -2,7 +2,14 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const bodyParser = require('body-parser');
+const Yelp = require('yelp');
 
+var yelp = new Yelp({
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET
+});
 
 // setup db
 const port = process.env.PORT || 5000;
@@ -22,7 +29,21 @@ app.get('/', (request, response) => {
   response.json({hello: 'world'});
 });
 
-
+app.get('/api/nightlife', (request, response) => {
+  if (!request.query.lat || !request.query.lon) {
+    response.json({status: "error", message: "coordinates not provided"});
+  }
+  
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  yelp.search({term: 'nightlife', ll: lat + ',' + lon})
+    .then((data) => {
+      response.json({status: "success", message: "", data});
+    })
+    .catch(() => {
+      response.json({status: "error", message: "error contacting the yelp api"});
+    })
+});
 
 MongoClient.connect(mongolabUri, (err, database) => {
   if (err) return console.log(err)
