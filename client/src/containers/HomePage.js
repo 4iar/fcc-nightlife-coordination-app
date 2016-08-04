ce2239789a87764fafb063de7a77207ab93ea536
4iar <4iar@users.noreply.github.com>
2016-08-04 01:54:06 +0100

    Add geosuggest styling

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Geosuggest from 'react-geosuggest';
 
-
-
 import Venue from '../components/Venue';
+
+import 'react-geosuggest/module/geosuggest.css';
+
 
 export default class HomePage extends React.Component {
   constructor(props) {
